ec400fefc841791e4a14bc4d25dfe596bc51c8a7
Alexey Petrushin <petrushinalex@gmail.com>
2021-03-25 22:21:18 +0300

    AnimationManager

---
+++ TMessagesProj/src/main/java/org/telegram/ui/Animations/AnimationManager.java
@@ -0,0 +1,67 @@
+package org.telegram.ui.Animations;
+import android.content.Context;
+
+import androidx.collection.ArrayMap;
+
+import org.telegram.messenger.ApplicationLoader;
+import org.telegram.messenger.FileLog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class AnimationManager {
+    Map<String, List<AnimationDataListener>> mapListeners = new ArrayMap<>();
+
+    AnimationPreferences preferences;
+
+    private AnimationManager(Context context) {
+        this.preferences = new AnimationPreferences(context);
+    }
+
+    private static volatile AnimationManager Instance = null;
+    public static AnimationManager getInstance() {
+        AnimationManager localInstance = Instance;
+        if (localInstance == null) {
+            synchronized (AnimationManager.class) {
+                localInstance = Instance;
+                if (localInstance == null) {
+                    Instance = localInstance = new AnimationManager(ApplicationLoader.applicationContext);
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    public interface AnimationDataListener {
+        void onAnimationDataUpdate(String key, Object value);
+    }
+
+    public void notify(String key, Object value) {
+        List<AnimationDataListener> listeners = mapListeners.get(key);
+        if (listeners != null) {
+            for (AnimationDataListener listener: listeners) {
+                listener.onAnimationDataUpdate(key, value);
+            }
+        }
+    }
+
+    private void addListener(String key, AnimationDataListener listener) {
+        List<AnimationDataListener> listeners = mapListeners.get(key);
+        if (listeners == null) {
+            listeners = new ArrayList<>();
+            mapListeners.put(key, listeners);
+        }
+        if (!listeners.contains(listener)) {
+            listeners.add(listener);
+        }
+    }
+
+    private void removeListener(String key, AnimationDataListener listener) {
+        List<AnimationDataListener> listeners = mapListeners.get(key);
+        if (listeners != null) {
+            listeners.remove(listener);
+        }
+    }
+
+}
